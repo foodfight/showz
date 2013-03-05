2d87374a139f3324094117d436f522aef650545b
nathenharvey <nharvey@opscode.com>
2013-03-05 10:54:44 -0500

    Update scripts for finding gems and gem lists for 5-mar-2013

--- whats-cookin/chef_gems/find_chef_gems.sh
+++ whats-cookin/chef_gems/find_chef_gems.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gem search -r chef | grep -v knife- | grep -v REMOTE | awk NF
+gem search -r '*chef' | grep -v knife- | grep -v bookchef | grep -v REMOTE | awk NF
 gem search -r berkshelf | grep -v REMOTE | awk NF
 gem search -r ridley | grep -v REMOTE | awk NF
