f1d0855c072e2d7521f2b0b89c7e33c69927c5dd
nathenharvey <nharvey@opscode.com>
2013-03-25 08:47:03 -0400

    add foodcritic and jvmargs to chef gems

--- whats-cookin/chef_gems/find_chef_gems.sh
+++ whats-cookin/chef_gems/find_chef_gems.sh
@@ -2,3 +2,5 @@
 gem search -r '*chef' | grep -v knife- | grep -v bookchef | grep -v REMOTE | awk NF
 gem search -r berkshelf | grep -v REMOTE | awk NF
 gem search -r ridley | grep -v REMOTE | awk NF
+gem search -r foodcritic | grep -v REMOTE | awk NF
+gem search -r jvmargs | grep -v REMOTE | awk NF
