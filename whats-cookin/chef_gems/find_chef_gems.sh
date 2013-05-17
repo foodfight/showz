053d062f113dbacfbabf172fb76090ecc179f5e7
nathenharvey <nharvey@opscode.com>
2013-05-17 14:15:46 -0400

    add busser to the list of chef gems

--- whats-cookin/chef_gems/find_chef_gems.sh
+++ whats-cookin/chef_gems/find_chef_gems.sh
@@ -7,3 +7,4 @@ gem search -r jvmargs | grep -v REMOTE | awk NF
 gem search -r vagrant-berkshelf | grep -v REMOTE | awk NF
 gem search -r kitchen | grep -v kitchensink | grep -v REMOTE | awk NF
 gem search -r strainer | grep -v REMOTE | awk NF
+gem search -r busser | grep -v REMOTE | awk NF
