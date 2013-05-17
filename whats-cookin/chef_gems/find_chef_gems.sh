59c4f266a4cf5173822be17756f7620b18ee6d2f
nathenharvey <nharvey@opscode.com>
2013-05-17 09:28:44 -0400

    update the script to find chef gems

--- whats-cookin/chef_gems/find_chef_gems.sh
+++ whats-cookin/chef_gems/find_chef_gems.sh
@@ -5,5 +5,5 @@ gem search -r ridley | grep -v REMOTE | awk NF
 gem search -r foodcritic | grep -v REMOTE | awk NF
 gem search -r jvmargs | grep -v REMOTE | awk NF
 gem search -r vagrant-berkshelf | grep -v REMOTE | awk NF
-gem search -r kitchen | grep -v REMOTE | awk NF
+gem search -r kitchen | grep -v kitchensink | grep -v REMOTE | awk NF
 gem search -r strainer | grep -v REMOTE | awk NF
