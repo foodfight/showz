8f8f6879280fcf4a5299bea19b980c2f4402b0fb
nathenharvey <nharvey@opscode.com>
2013-06-06 09:49:32 -0700

    add more chef-related gems to the search
    * fog
    * right_aws

--- whats-cookin/chef_gems/find_chef_gems.sh
+++ whats-cookin/chef_gems/find_chef_gems.sh
@@ -9,3 +9,5 @@ gem search -r kitchen | grep -v kitchensink | grep -v REMOTE | awk NF
 gem search -r strainer | grep -v REMOTE | awk NF
 gem search -r busser | grep -v REMOTE | awk NF
 gem search -r emeril | grep -v REMOTE | awk NF
+gem search -r fog | grep -v REMOTE | awk NF
+gem search -r right_aws | grep -v REMOTE | awk NF
